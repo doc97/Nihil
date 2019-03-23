eef59c6283a9ef1aa222c755f1ee99ee3211d2f7
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-03-23 23:48:16 +0200

    Add background textures and character

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float maxSpeed = 10f;
+    
+    private Rigidbody2D body;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float movement = Input.GetAxis("Move");
+
+        body.velocity = new Vector2(movement * maxSpeed, body.velocity.y);
+    }
+}
---
+++ Assets/Scripts/FollowObjectController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObjectController : MonoBehaviour
+{
+    public GameObject target;
+    public Vector3 offset = new Vector3();
+    public bool followX = true;
+    public bool followY = true;
+    public bool followZ = true;
+
+    void LateUpdate()
+    {
+        Vector3 newPos = target.transform.position + offset;
+        float x = followX ? newPos.x : transform.position.x;
+        float y = followY ? newPos.y : transform.position.y;
+        float z = followZ ? newPos.z : transform.position.z;
+
+        transform.position = new Vector3(x, y, z);
+    }
+}
