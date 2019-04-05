0e382e1fd02cb7bf36ca43c36983c3af35bbb4bc
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-04-05 19:36:35 +0300

    Flip animation when character moves

--- Assets/Scripts/Behaviours/CharacterMovement.cs
+++ Assets/Scripts/Behaviours/CharacterMovement.cs
@@ -16,7 +16,16 @@ public class CharacterMovement : MonoBehaviour
     void FixedUpdate()
     {
         float movement = Input.GetAxis("Move");
-
         body.velocity = new Vector2(movement * maxSpeed, body.velocity.y);
+
+        // Don't change face direction if standing still
+        if (movement != 0)
+            SetFaceDirection(movement);
+    }
+
+    private void SetFaceDirection(float movement) {
+        Vector3 currentScale = transform.localScale;
+        currentScale.x = Mathf.Sign(movement);
+        transform.localScale = currentScale;
     }
 }
