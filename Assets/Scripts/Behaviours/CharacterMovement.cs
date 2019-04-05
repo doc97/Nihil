292abb06bcec34d52020e7d7b0d74faddc1491a6
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-04-05 20:25:42 +0300

    Add idle state, tweak character speed

--- Assets/Scripts/Behaviours/CharacterMovement.cs
+++ Assets/Scripts/Behaviours/CharacterMovement.cs
@@ -7,25 +7,49 @@ public class CharacterMovement : MonoBehaviour
     public float maxSpeed = 10f;
     
     private Rigidbody2D body;
+    private Animator animator;
+
+    private bool isMoving;
 
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
         float movement = Input.GetAxis("Move");
         body.velocity = new Vector2(movement * maxSpeed, body.velocity.y);
+        animator.speed = Mathf.Abs(body.velocity.x / maxSpeed);
 
-        // Don't change face direction if standing still
-        if (movement != 0)
-            SetFaceDirection(movement);
+        if (movement == 0 && isMoving)
+        {
+            isMoving = false;
+            OnStop();
+        }
+
+        if (movement != 0 && !isMoving)
+        {
+            isMoving = true;
+            OnMove();
+        }
+
+        SetFaceDirection(movement);
+    }
+
+    private void OnStop()
+    {
+        animator.SetBool("IsMoving", false);
+    }
+
+    private void OnMove() {
+        animator.SetBool("IsMoving", true);
     }
 
     private void SetFaceDirection(float movement) {
         Vector3 currentScale = transform.localScale;
-        currentScale.x = Mathf.Sign(movement);
+        currentScale.x = movement == 0 ? currentScale.x : Mathf.Sign(movement);
         transform.localScale = currentScale;
     }
 }
