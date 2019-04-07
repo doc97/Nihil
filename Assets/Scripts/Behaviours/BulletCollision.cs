314fefc3cefcd7968f3d694b63e1439bf7fccfb7
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-04-07 20:13:55 +0300

    Update bullet, add game area boundary

---
+++ Assets/Scripts/Behaviours/BulletCollision.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCollision : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        string tag = collision.gameObject.tag;
+        if (tag == "Obstacle")
+            Destroy(gameObject);
+    }
+}
