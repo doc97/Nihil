a87912f11251a0fa5d6ca85c83462dae26436bb4
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-04-07 20:36:34 +0300

    Add exit level functionality

--- Assets/Scripts/Behaviours/FadeController.cs
+++ Assets/Scripts/Behaviours/FadeController.cs
@@ -1,36 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 
 public class FadeController : MonoBehaviour
 {
-    public GameObject fadeInTarget;
+    public GameObject fadeTarget;
+    public GameObject character;
+    public Transform exitTrigger;
+
+    private SpriteRenderer targetRenderer;
+    private bool triggerOnFadeIn;
+    private bool triggerOnFadeOut;
 
     void Start()
     {
+        targetRenderer = fadeTarget.GetComponent<SpriteRenderer>();
         FadeIn();
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.J))
-        {
-            FadeIn();
-        }
-        else if (Input.GetKeyDown(KeyCode.K))
-        {
+        if (character.transform.position.x > exitTrigger.position.x)
             FadeOut();
-        }
+        
+        if (triggerOnFadeIn && targetRenderer.color.a == 0)
+            OnFadeIn();
+        if (triggerOnFadeOut && targetRenderer.color.a == 1)
+            OnFadeOut();
     }
 
     private void FadeIn()
     {
-        ExecuteEvents.Execute<IFadeListener>(fadeInTarget, null, (x, y) => x.StartFadeIn());
+        triggerOnFadeIn = true;
+        ExecuteEvents.Execute<IFadeListener>(fadeTarget, null, (x, y) => x.StartFadeIn());
     }
 
     private void FadeOut()
     {
-        ExecuteEvents.Execute<IFadeListener>(fadeInTarget, null, (x, y) => x.StartFadeOut());
+        triggerOnFadeOut = true;
+        ExecuteEvents.Execute<IFadeListener>(fadeTarget, null, (x, y) => x.StartFadeOut());
+    }
+
+    private void OnFadeIn()
+    {
+        triggerOnFadeIn = false;
+    }
+
+    private void OnFadeOut()
+    {
+        triggerOnFadeOut = false;
+        SceneManager.LoadScene("Intro", LoadSceneMode.Single);
     }
 }
