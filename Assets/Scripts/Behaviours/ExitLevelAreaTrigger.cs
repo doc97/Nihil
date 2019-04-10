d4121fa46e0212ae5bc263f5fca9086bebb269c3
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-04-10 14:57:27 +0300

    Decouple scene controller and character

---
+++ Assets/Scripts/Behaviours/ExitLevelAreaTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitLevelAreaTrigger : MonoBehaviour
+{
+    public GameObject character;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject == character)
+            Commands.FireFadeOut();
+    }
+}
