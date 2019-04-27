017b236b4d0d415ef1b4b4707c506f478b8d2a8d
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-04-27 21:56:04 +0300

    Update character sprite, add proper aiming

--- Assets/Scripts/Behaviours/CharacterShooting.cs
+++ Assets/Scripts/Behaviours/CharacterShooting.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 public class CharacterShooting : MonoBehaviour
 {
     public float bulletSpeed;
+    public Transform hand;
+    public Transform elbow;
+    public Transform shoulder;
 
+    private float shoulderToElbowDistance;
+    private float elbowAngleRad;
     private bool isShooting;
+
+    void Start()
+    {
+        Vector2 elbowToHand = hand.position - elbow.position;
+        Vector2 elbowToShoulder = shoulder.position - elbow.position;
+        shoulderToElbowDistance = elbowToShoulder.magnitude;
+        elbowAngleRad = Mathf.Deg2Rad * Vector3.Angle(elbowToHand, elbowToShoulder);
+    }
 
     void Update() {
         if (Input.GetButtonDown("Shoot"))
             isShooting = true;
     }
+
     void FixedUpdate()
     {
+        UpdateArmRotation();
         if (isShooting)
         {
             isShooting = false;
@@ -21,18 +36,27 @@ public class CharacterShooting : MonoBehaviour
         }
     }
 
+    private void UpdateArmRotation()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 jointPos = shoulder.position;
+        float distance = (mousePos - jointPos).magnitude;
+        float deltaY = mousePos.y - jointPos.y;
+
+        float angleRad = Mathf.Asin(Mathf.Sin(elbowAngleRad) * shoulderToElbowDistance / distance) + Mathf.Asin(deltaY / distance);
+        float angleDeg = angleRad * Mathf.Rad2Deg;
+        shoulder.rotation = Quaternion.AngleAxis(angleDeg, Vector3.forward);
+    }
+
     private void Shoot() {
         Vector3 direction = calculateBulletDirection();
-        Vector3 spawnPos = transform.position;
         GameObject bulletPrefab = Resources.Load("Prefabs/Bullet", typeof(GameObject)) as GameObject;
-        spawnBullet(bulletPrefab, spawnPos, direction);
+        spawnBullet(bulletPrefab, hand.position, direction);
     }
 
     private Vector3 calculateBulletDirection()
     {
-        Vector3 jointPos = transform.position;
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 direction = mousePos - jointPos;
+        Vector3 direction = hand.position - elbow.position;
         direction.z = 0;
         return direction.normalized;
     }
